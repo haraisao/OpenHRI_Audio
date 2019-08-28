50049d92594460022e33a9a0a1df999cb8cd276a
Isao Hara <haraisao@gmail.com>
2019-08-28 10:41:46 +0900

    Update linker option (DSArray)

--- env.bat
+++ env.bat
@@ -1,9 +1,9 @@
 
 @if "%1" == "" @(
-    @set DRV=E
+    @set DRV=%~d0
 ) else (
     @set DRV=%1
 )
 
-@call %DRV%:\local\OpenRTM-aist\setup.bat
+@call %DRV%\local\OpenRTM-aist\setup.bat
 @set PATH=%~dp0bin;%PATH%
