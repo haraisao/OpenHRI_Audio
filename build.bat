87347cdba78a01f4467d50259c1640d21a778359
Isao Hara <haraisao@gmail.com>
2019-08-29 10:16:34 +0900

    Add /utf-8 option

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cmake -G "Visual Studio 16 2019" -A x64 -B build -S .
+cmake -G "Visual Studio 16 2019" -A x64 -B build -S . -DOPENRTM_VERSION=1.2.1 -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release --target INSTALL
